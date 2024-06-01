c4691b9639d8ad428519b83de6f715a605f59182
Kaled Enríquez <kaledtrejo@hotmail.com>
2024-06-01 06:10:08 -0600

    about me section

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -5,19 +5,35 @@ import { PiLinkedinLogoFill, PiGithubLogoFill, PiDownloadSimple } from "react-ic
 
 export default function About() {
   return (
+<>
     <div className={styles.socials}>
-        <Link className={styles.icon} href="www.linkedin.com/in/kaled-enriquez">
+        <Link className={styles.icon} href="https://www.linkedin.com/in/kaled-enriquez" target="_blank">
             <PiLinkedinLogoFill />
         </Link>
-        <Link className={styles.icon} href="https://github.com/kaledenriquez">
+        <Link className={styles.icon} href="https://github.com/kaledenriquez" target="_blank">
             <PiGithubLogoFill />
         </Link>
-        <Link style={{textDecoration: 'none'}} href="/kaledEnríquez_Resume.pdf">
+        <Link style={{textDecoration: 'none'}} href="/kaledEnríquez_Resume.pdf" target="_blank">
             <Button title="Download CV" icon={<PiDownloadSimple />}/>
         </Link>
     </div>
 
-    
+    <div className={styles.aboutSection}>
+        <div className={styles.aboutHeader}> 
+            <h1>About Me</h1>
+            <div className={styles.underline}></div>
+        </div>
+        <div className={styles.aboutContent}>
+            <p>
+                I'm currently studying computer science at Tecnológico de Monterrey,
+                but I'm also a person who loves to learn by himself and keep trying new things.
+                I'm always looking for new challenges and opportunities to hone my skills and
+                bring the most value I can to the table.
+            </p>
+        </div>
+    </div>
+</>
+
   )
 }
 
