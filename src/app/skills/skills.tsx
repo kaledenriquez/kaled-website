94a0e05af212a4876c521e46d9bdd0dabf55b025
Skalitz <a01198666@tec.mx>
2024-06-03 14:59:43 -0600

    minor changes

--- src/app/skills/skills.tsx
+++ src/app/skills/skills.tsx
@@ -2,6 +2,16 @@ import styles from "@/src/app/styles/skills.module.css"
 
 export default function Skills(){
     return(
-        <div>dadsasadad</div>
+<>
+    <div className={styles.skillsSection}>
+            <div className={styles.skillsHeader}> 
+                <h1>My Skills</h1>
+                <div className={styles.underline}></div>
+            </div>
+            <div className={styles.skillsContent}>
+
+            </div>
+        </div>
+</>
     )
 }
